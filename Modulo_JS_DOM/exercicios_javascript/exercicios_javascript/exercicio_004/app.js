a3739690b20d681db47a909907077b034471d997
MillerVasco <millermartinsvasco1@gmail.com>
2023-05-12 13:20:03 -0300

    1320

--- Modulo_JS_DOM/exercicios_javascript/exercicios_javascript/exercicio_004/app.js
+++ Modulo_JS_DOM/exercicios_javascript/exercicios_javascript/exercicio_004/app.js
@@ -9,3 +9,29 @@ Enunciado: TRÊS SLIDERS COM VALORES INDEPENDENTES
     definidas através do JavaScript.
 
 ---------------------------------------------------------------------------- */
+// value_1
+// range_1
+
+const range_1 = document.querySelector("#range_1")
+range_1.setAttribute('min', 0)
+range_1.setAttribute('max', 100)
+range_1.value = 0;
+range_1.addEventListener('input', (event) =>{
+    document.querySelector("#value_1").textContent = event.target.value;
+})
+
+const range_2 = document.querySelector("#range_2")
+range_2.setAttribute('min', 0)
+range_2.setAttribute('max', 100)
+range_2.value = 0;
+range_2.addEventListener('input', (event) =>{
+    document.querySelector("#value_2").textContent = event.target.value;
+})
+
+const range_3 = document.querySelector("#range_3")
+range_3.setAttribute('min', 0)
+range_3.setAttribute('max', 100)
+range_3.value = 0;
+range_3.addEventListener('input', (event) =>{
+    document.querySelector("#value_3").textContent = event.target.value;
+})
