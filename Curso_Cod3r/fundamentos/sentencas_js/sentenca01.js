6e7b9b886f9481c0539028c768b0c8b38adeb760
MillerVasco <millermartinsvasco1@gmail.com>
2023-05-18 20:01:36 -0300

    2001

---
+++ Curso_Cod3r/fundamentos/sentencas_js/sentenca01.js
@@ -0,0 +1,5 @@
+console.log('legal');
+/* Uma sentença de codifo em js pode ou não terminar com ;*/
+console.log('Uma sentença de codifo em js pode ou não terminar com ;');
+
+// as sentenças de codigo são a base de um codigo de programação.
