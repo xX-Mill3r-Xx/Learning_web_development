c81aee344d292a4b33de100cf4d4b812ef6f7318
MillerVasco <millermartinsvasco1@gmail.com>
2023-05-23 14:06:44 -0300

    1406

---
+++ Curso_Cod3r/Html/assets/js/index.js
@@ -0,0 +1,8 @@
+document.querySelectorAll("[wm-nav]").forEach(link => {
+    const conteudo = document.getElementById('conteudo')
+    link.onclick = function(e){
+        e.preventDefault()
+        fetch(link.getAttribute('wm-nav')).then(resp => resp.text())
+            .then(html => conteudo.innerHTML = html)      
+    }
+})
